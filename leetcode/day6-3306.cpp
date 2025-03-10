453d74d9a0f379cd89ff821aceea3ebeed1386a4
Aayush-Duhan <aayushduhan82@gmail.com>
2025-03-10 22:44:55 +0530

    Add solution for Count of Substrings with All Vowels in LeetCode

---
+++ leetcode/day6-3306.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long countOfSubstrings(string word, int k) {
+        auto f = [&](int k) -> long long {
+            long long ans = 0, left = 0, vowel = 0, consonants = 0;
+            vector<int> freq(256, 0);
+
+            for (char &w : word) {
+                if (w != 'a' && w != 'e' && w != 'i' && w != 'o' && w != 'u') 
+                    consonants++;
+                else if (++freq[w] == 1) 
+                    vowel++;
+                
+                while ((vowel == 5) && (consonants >= k)) {
+                    char c = word[left++];
+                    if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') 
+                        consonants--;
+                    else if (--freq[c] == 0) 
+                        vowel--;
+                }
+                ans += left;
+            }
+            return ans;
+        };
+
+        return f(k) - f(k + 1);
+    }
+};
